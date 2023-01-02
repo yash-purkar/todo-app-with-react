669bff34fdd49a79098aca704613f9696be01aa3
yashpurkar <yashpurkar7079@gmail.com>
2023-01-02 11:17:06 +0530

    initial commit

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Todo = () => {
   return (
-    <div>Todo</div>
+    <div className='main-div'>
+      <div className="child-div">
+        <figure>
+          <img src="" alt="" />
+          <figcaption>Add Your List Here✍</figcaption>
+        </figure>
+
+        <div className="inputDiv">
+          <input type="text" />
+          <button>+</button>
+        </div>
+
+        <div className="showList">
+          <div className="item">
+            <h3>Apple</h3>
+            <div className="two-buttons">
+              <button>edit</button>
+              <button>delete</button>
+            </div>
+          </div>
+        </div>
+        <div className="clear-btn">
+          <button>Clear All</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
